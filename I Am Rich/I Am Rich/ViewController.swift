9c6b2bde68a8a8cd81d693b1bf4b0a91a9190d14
emilshouse <emilshouse@gmail.com>
2019-10-30 22:00:23 -0700

    first project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Adnan Abdulai on 10/30/19.
+//  Copyright © 2019 Adnan Abdulai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
