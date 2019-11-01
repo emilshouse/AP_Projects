e51ca07896b209868cc68319394e885c7f0391a6
emilshouse <emilshouse@gmail.com>
2019-10-31 20:33:37 -0700

    2nd project added.

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Adnan Abdulai on 10/31/19.
+//  Copyright © 2019 Adnan Abdulai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
