95f28c487f26fca8c68b3e30e417c9e463e29bf5
emilshouse <emilshouse@gmail.com>
2019-11-05 21:03:20 -0800

    Adding Xylophone project

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        if let title = sender.titleLabel?.text {
+        playSound(title)
+    }
+    }
+
+    func playSound(_ key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+
+    }
+}
+
+
