e70543066cb24e3cb944c27440c28d1d89f4d0e3
emilshouse <emilshouse@gmail.com>
2019-11-15 20:23:38 -0800

    Adding more functionality to the app

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -14,21 +14,32 @@ class ViewController: UIViewController {
     let mediumTime = 7
     let hardTime = 12
 
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
 
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        switch sender.currentTitle {
-        case "Soft":
-            print(eggTimes["Soft"]!)
-        case "Medium":
-            print(eggTimes["Medium"]!)
-        case "Hard":
-            print(eggTimes["Hard"]!)
-        default:
-            print("Hardness out of bounds")
+        var interval = eggTimes[sender.currentTitle!]!
+//        switch sender.currentTitle {
+//        case "Soft":
+//            print(eggTimes["Soft"]!)
+//        case "Medium":
+//            print(eggTimes["Medium"]!)
+//        case "Hard":
+//            print(eggTimes["Hard"]!)
+//        default:
+//            print("Hardness out of bounds")
+//        }
+
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if interval > 0 {
+                print("\(interval) seconds")
+                interval -= 1
+            }
+
         }
     }
+
+
 
 
 
