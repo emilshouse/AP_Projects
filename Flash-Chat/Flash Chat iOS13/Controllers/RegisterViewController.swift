0611287e0ce3442567109f1469b586b669ed6493
emilshouse <emilshouse@gmail.com>
2020-02-04 20:26:34 -0800

    add Firebase to FlashChat

--- Flash-Chat/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+
+
     
     @IBAction func registerPressed(_ sender: UIButton) {
+
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                if let e = error {
+                    print(e.localizedDescription)
+                    let ac = UIAlertController(title: "Error", message: e.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(ac, animated: true)
+                } else {
+                    //Navigate to the chat view controller.
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    
+                }            }
+
+        }
+
     }
     
 }
--- Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -16,6 +16,7 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
         titleLabel.text = "⚡️FlashChat"
 
 //        titleLabel.text = ""
