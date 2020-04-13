08ba830ee8844f7263b3c024edbf174becba7daf
emilshouse <emilshouse@gmail.com>
2020-04-12 20:54:22 -0700

    Adding Calculater project

---
+++ Structs vs. Classes/Structs vs. Classes/StuctExample.swift
@@ -0,0 +1,9 @@
+//
+//  StuctExample.swift
+//  Structs vs. Classes
+//
+//  Created by Admin on 4/12/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
---
+++ Structs vs. Classes/Structs vs. Classes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Structs vs. Classes
+//
+//  Created by Admin on 4/12/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
